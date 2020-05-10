49674188ef42b65a940163037908e403759e2af0
kenaryn <aurelien.plazzotta@protonmail.com>
2020-05-10 11:21:24 +0200

    Refactorization, add last checking and a new error redirection

--- zig-update.zsh
+++ zig-update.zsh
@@ -15,74 +15,89 @@ ARCH="$(uname --kernel-name --machine | sed 's/\s/-/')" # Outputs 'Linux-x86_64'
 SRV_RESPONSE=$(curl --silent --write-out "%{http_code}\n" --location "${HOST}/" \
     --output "/dev/null")
 
-function move_zig() {
+function download() {
+    # Use `jq` to silently fetch last version or fallback to `grep` otherwise
+    if whence jq > /dev/null; then
+        API=(jq -r '.master.version')
+    else
+        API=(grep -Po '(?<="version": ")[^"]*')
+    fi
+
+    function {
+        local JSON='https://ziglang.org/download/index.json'
+        curl -fsSL "$JSON" | $@
+    } ${API}
+
+    # `:l` modifier converts 'ARCH' value to lowercase letters.
+    FILE="${ARCH:l}-${FILE}.tar.xz"
+    curl -O "https://ziglang.org/builds/${FILE}"
+
+}
+
+function extract() {
+    tar -xJf "${INSTALL_DIR}/${FILE}"
+    # Take the longest match before '.tar' substring (i.e. strip off new dir from its extension)
+    mv "${FILE%%\.tar*}" 'zig'
     mv 'zig' "${HOME}/.local/bin"
 }
 
 if [[ ($SRV_RESPONSE == (200 || 403) ]]; then
-    if grep -qF '/bin/zig' "${HOME}/.zshrc"; then
+    if grep -qF '/bin/zig' "${HOME}/.zshrc"; then # If Zig already installed
         MYVERSION="$(zig version)"
         if ( MYVERSION == FILE); then
             print "Zig is already the newest version (${MYVERSION}).\n \
             There is nothing to upgrade.\n"
             exit 0
         fi
-    else
+
         # Check out if zig is running or runnable in the foreground, whether associated to
         # the terminal or not, before cleaning up the cache and removing the compiler old's version
-        # move 'build' sub-string to a case to test other commands like fmt, cc, build-exe, etc.
+        # Test against 'build' sub-string to a case loop to include other zig's commands (e.g. fmt, cc, build-exe, etc.)
         if [[ -d "${HOME}/.local/cache/zig" ]] && \
         [[ -z "$(ps -d -u ${USERNAME} -o stat,command | grep '^R+[[:blank:]]\+zig build')" ]]; then
-            rm -rf "${HOME}/.local/{cache,bin}/zig"
-            STATUS='updated'
+            rm -rf "${HOME}/.local/{cache,bin}/zig" # TODO: make it an archive to stash it instead of removing it to reverse changes if installation fails
+
+            download
+            extract
+
+            source "${HOME}/.zshrc"
+
+            # Last check
+            if zig version > /dev/null; then
+                rm -f "${INSTALL_DIR}/${FILE}"
+                STATUS='Zig has been updated successfully.\n' # TODO: add a uninstallation' scenario
+                print "${STATUS}Type 'zig --help' to try out available options.\n"
+                exit 0
+            else
+                STATUS='Zig has not been properly updated.\n'
+                print "{STATUS}Your downloaded archive has been preserved. Please try another method." >&2
+                exit 2
+            fi
+
         else
-            printf %s\\n "Zig is currently running (or in is run queue). Hence, is it UNSAFE to \
+            printf '%s\\n' "Zig is currently running (or in is run queue). Hence, is it UNSAFE to \
             perform any update for now. Please retry after the current zig process's \
             completion." >&2
-                exit 1
+            exit 1
         fi
 
-        # Use `jq` to silently fetch last version or fallback to `grep` otherwise
-        # if the former is not installed
-        if whence jq > /dev/null; then
-            API=(jq -r '.master.version')
-        else
-            API=(grep -Po '(?<="version": ")[^"]*')
-        fi
-
-        function {
-            local JSON='https://ziglang.org/download/index.json'
-            curl -fsSL "$JSON" | $@
-        } ${API}
-
-        # `:l` modifier converts 'ARCH' to lowercase letters.
-        FILE="${ARCH:l}-${FILE}.tar.xz"
-        curl -O "https://ziglang.org/builds/${FILE}"
-        tar -xJf "${INSTALL_DIR}/${FILE}"
-        # Take the longest match before '.tar' substring (i.e. strip off new dir from its extension)
-        mv "${FILE%%\.tar*}" 'zig'
+    else
         if [[ ! -d "${HOME}/.local/bin" ]]; then
             mkdir -p "${HOME}/.local/bin"
-            move_zig
         fi
         if [[ ! -w "${HOME}/.local/bin" ]]; then
             chmod u+x "${HOME}/.local/bin"
-            move_zig
-        fi
-        chmod u+x "${HOME}/.local/bin/zig/zig"
-
-        # In case of very first Zig's installation
-        if ! grep -qF '/bin/zig' "${HOME}/.zshrc"; then # Reversed test already exist, MAY be moved to a clause elif?
-            # Replace the line below by superior zsh syntax (i.e. path+=() and typeset)
-            printf %s\\n "export PATH=${PATH}:${HOME}/.local/bin/zig" >> "${HOME}/.zshrc"
-            STATUS='installed'
         fi
 
+        download
+        extract
+
+        # run-control file do not have access to $HOME, that's why the path is replaced with '~'
+        printf '%s\\n' "path+=('~/.local/bin/zig')" >> "${HOME}/.zshrc"
+        printf '%s\\n' "typeset -TUx PATH path" >> "${HOME}/.zshrc"
         source "${HOME}/.zshrc"
-        rm -f "${INSTALL_DIR}/${FILE}"
-        print "Zig has been ${STATUS} successfully.\nType 'zig --help' to try out \
-        available options.\n"
-        exit 0
+        chmod u+x "${HOME}/.local/bin/zig/zig"
+        STATUS='Zig has been installed successfully.\n'
     fi
 else
     # TODO: make the logging book a function to be reused for each non-null exit codes.
@@ -94,11 +109,7 @@ else
 fi
 
 #  TODO:
-#  2)Check for various shell to make it more widely usable (e.g. `if .bashrc || .tcshrc`).
-#  3)Add standard error outputs redirection for logging purpose.
-#  4)Add other available architectures to target a broader audience (e.g. x86_64-freebsd users
-#+ in the JSON download-related online file, `setenv` instead of `export` for tcsh, `curl` won't
-#+ work on Windows, etc.).
+#  4)Add other available architectures to target a broader audience (e.g. x86_64-dragonfly, freebsd users)
 #  6)Replace checking with $HOME/.zshrc file with PATH env variable (more robust in case a zig word
 #+ unrelated to the language programming is in the same way found in the run-control file!).
 #  7)Make the script system-wide executable by moving it to '/usr/bin' (see IPFS `install.sh`
